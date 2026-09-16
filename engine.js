b7f702362c34da9c651b68eac16252551751cc76
kyle8824 <kyle8824@gmail.com>
2026-09-16 12:08:59 -0400

    v0.9.1: export wildlife trace perception

--- engine.js
+++ engine.js
@@ -2,4 +2,4 @@ export{WORLD_VERSION,PHYSICS_VERSION,createWorld,migrateWorld,addEvent,remember,
 export{WORLD_MODEL_VERSION,ensureWorldModel,findWorldObject,worldObjectForZone,activeWorldObjects,instantiateObjectComponents,detachComponent,objectSnapshot}from'./engine/world-model.js';
 export{PHYSICAL_VERBS,PHYSICAL_CONFIGURATIONS,deriveSupports,buildAffordanceView,normalizePhysicalProposal,validatePhysicalProposal}from'./engine/affordances.js';
 export{candidateActions,retrieveDecisionContext,tickWithMind,tick}from'./engine/runtime.js';
-export{ECOLOGY_VERSION,ensureEcology,advanceEcology,visibleWildlifeForAgent,wildlifeAIEnabled}from'./engine/ecology.js';
+export{ECOLOGY_VERSION,ensureEcology,advanceEcology,visibleWildlifeForAgent,visibleWildlifeTracesForAgent,wildlifeAIEnabled}from'./engine/ecology.js';
